fcc3d955a9d98766dc665283b43fe4283acebc1a
Anastasios Andronidis <anastasios.andronidis@toolsforhumanity.com>
2024-10-05 02:40:35 +0200

    Implement From for various types (#33)

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,33 @@
 pub use ::prost;
+pub use ::prost_types;
 pub use ::tonic;
 
 pub mod relay {
     tonic::include_proto!("relay");
+
+    impl From<RelayPayload> for RelayConnectRequest {
+        fn from(payload: RelayPayload) -> Self {
+            RelayConnectRequest {
+                msg: Some(relay_connect_request::Msg::Payload(payload)),
+            }
+        }
+    }
+
+    impl From<Heartbeat> for RelayConnectRequest {
+        fn from(heartbeat: Heartbeat) -> Self {
+            RelayConnectRequest {
+                msg: Some(relay_connect_request::Msg::Heartbeat(heartbeat)),
+            }
+        }
+    }
+
+    impl From<ConnectRequest> for RelayConnectRequest {
+        fn from(connect_request: ConnectRequest) -> Self {
+            RelayConnectRequest {
+                msg: Some(relay_connect_request::Msg::ConnectRequest(connect_request)),
+            }
+        }
+    }
 }
 
 pub mod common {
