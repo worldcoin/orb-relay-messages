33798706af07d0da5f9427276b2276c690092dd9
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2025-01-27 18:26:13 +0100

    tests(relay-client): e2e reply

--- relay-client/tests/test_server.rs
+++ relay-client/tests/test_server.rs
@@ -1,13 +1,15 @@
 use orb_relay_messages::relay::{
     relay_connect_request,
-    relay_connect_response::Msg,
+    relay_connect_response::{self, Msg},
     relay_service_server::{RelayService, RelayServiceServer},
-    Ack, ConnectResponse, RelayConnectRequest, RelayConnectResponse, RelayPayload,
+    Ack, ConnectRequest, ConnectResponse, Entity, RelayConnectRequest,
+    RelayConnectResponse, RelayPayload,
 };
-use std::{net::SocketAddr, pin::Pin, sync::Arc};
+use std::{collections::HashMap, net::SocketAddr, pin::Pin, sync::Arc};
 use tokio::{
     net::TcpListener,
     sync::{Mutex, MutexGuard},
+    task,
 };
 use tokio_stream::{wrappers::TcpListenerStream, Stream};
 use tonic::{transport::Server, Request, Response, Status, Streaming};
@@ -15,17 +17,69 @@ use tonic::{transport::Server, Request, Response, Status, Streaming};
 type OutboundMessageStream =
     Pin<Box<dyn Stream<Item = Result<RelayConnectResponse, Status>> + Send>>;
 
+#[derive(Default, Clone)]
+pub struct ConnectedClients {
+    clients: Arc<Mutex<HashMap<String, flume::Sender<Cmd>>>>,
+}
+
+impl ConnectedClients {
+    async fn insert(&self, entity: &Entity, tx: flume::Sender<Cmd>) {
+        let mut clients = self.clients.lock().await;
+        let key = entity_key(entity);
+        clients.insert(key, tx.clone());
+    }
+
+    #[allow(dead_code)] // wtf, this is actually used lol...
+    pub fn send(&self, payload: RelayPayload) {
+        let clients = self.clients.clone();
+        task::spawn(async move {
+            let clients = clients.lock().await;
+            let entity_key = entity_key(payload.dst.as_ref().unwrap());
+            if let Some(tx) = clients.get(&entity_key) {
+                let _ = tx.send(Cmd::Send(payload));
+            }
+        });
+    }
+
+    #[allow(dead_code)]
+    pub fn stop(&self, entity: &Entity) {
+        let clients = self.clients.clone();
+        let entity_key = entity_key(&entity);
+
+        task::spawn(async move {
+            let clients = clients.lock().await;
+            if let Some(tx) = clients.get(&entity_key) {
+                let _ = tx.send(Cmd::Terminate);
+            }
+        });
+    }
+}
+
 struct RelayServiceHandler<S>
 where
     S: Send + Sync + 'static,
 {
     state: Arc<Mutex<S>>,
+    clients: ConnectedClients,
     handler: Arc<
-        dyn Fn(&mut S, relay_connect_request::Msg) -> Option<RelayConnectResponse>
+        dyn Fn(
+                &mut S,
+                relay_connect_request::Msg,
+                &ConnectedClients,
+            ) -> Option<RelayConnectResponse>
             + Send
             + Sync
             + 'static,
     >,
+}
+
+enum Cmd {
+    Send(RelayPayload),
+    Terminate,
+}
+
+fn entity_key(e: &Entity) -> String {
+    format!("{}{}{}", e.id, e.namespace, e.entity_type)
 }
 
 #[tonic::async_trait]
@@ -39,16 +93,49 @@ where
         &self,
         request: Request<Streaming<RelayConnectRequest>>,
     ) -> Result<Response<Self::RelayConnectStream>, Status> {
+        let (tx, rx) = flume::unbounded();
         let mut stream = request.into_inner();
         let state = self.state.clone();
         let handler = self.handler.clone();
+        let clients = self.clients.clone();
 
         let output = async_stream::stream! {
             loop {
-                let msg = stream.message().await.unwrap().unwrap().msg.unwrap();
-                let mut state = state.lock().await;
-                if let Some(res) = (handler)(&mut state, msg) {
-                    yield Ok(res)
+                let handled_replies = async {
+                    let msg = stream.message().await.unwrap().unwrap().msg.unwrap();
+
+                    if let relay_connect_request::Msg::ConnectRequest(ConnectRequest { client_id, ..   }) =  &msg {
+                        let entity = client_id.as_ref().unwrap();
+                        clients.insert(entity, tx.clone()).await;
+                    }
+
+                    let mut state = state.lock().await;
+                    (handler)(&mut state, msg, &clients)
+                };
+
+
+                tokio::select! {
+                    msg = handled_replies => {
+                        if let Some(m) = msg {
+                            yield Ok(m)
+                        }
+                    }
+
+                    msg = rx.recv_async() => {
+                        match msg.unwrap() {
+                            Cmd::Send(payload) => {
+                                let res = RelayConnectResponse {
+                                    msg: Some(relay_connect_response::Msg::Payload(payload)),
+                                };
+
+                                yield Ok(res)
+                            }
+
+                            Cmd::Terminate => {
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         };
@@ -63,6 +150,7 @@ where
 /// separate tokio task and will be shut down when this struct is dropped.
 pub struct TestServer<S> {
     addr: SocketAddr,
+    #[allow(dead_code)] // not dead code but clippy complains eh
     state: Arc<Mutex<S>>,
     shutdown: flume::Sender<()>,
 }
@@ -107,7 +195,11 @@ where
     /// ```
     pub async fn new<F>(state: S, handler: F) -> Self
     where
-        F: Fn(&mut S, relay_connect_request::Msg) -> Option<RelayConnectResponse>
+        F: Fn(
+                &mut S,
+                relay_connect_request::Msg,
+                &ConnectedClients,
+            ) -> Option<RelayConnectResponse>
             + Send
             + Sync
             + 'static,
@@ -123,6 +215,7 @@ where
             Server::builder()
                 .add_service(RelayServiceServer::new(RelayServiceHandler {
                     state: state.clone(),
+                    clients: ConnectedClients::default(),
                     handler,
                 }))
                 .serve_with_incoming_shutdown(
@@ -145,6 +238,7 @@ where
         self.addr
     }
 
+    #[allow(dead_code)] // this is not dead code why do i need this
     /// Returns a mutex guard that can be used to access and modify the server's state
     pub async fn state(&self) -> MutexGuard<'_, S> {
         self.state.lock().await
