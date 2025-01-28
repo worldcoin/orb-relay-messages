ecadd0bb7be3def6305df6bd2f3bba300f272462
vmenge <victor.ferreira.menge@toolsforhumanity.com>
2025-01-28 15:16:49 +0100

    chore(relay-client): commit hooks plz

--- relay-client/tests/send.rs
+++ relay-client/tests/send.rs
@@ -11,7 +11,7 @@ use orb_relay_messages::{
         ConnectResponse, Entity, RelayPayload,
     },
 };
-use std::{time::Duration};
+use std::time::Duration;
 use test_server::{IntoRes, TestServer};
 use tokio::time;
 
