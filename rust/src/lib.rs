48221ddeab8ff01ef9d88e9659952f3091d75ace
Matthew Paul <matthew.paul@toolsforhumanity.com>
2025-07-11 09:46:55 -0700

    Add new job subscriber domain model to separate client domain from subscriber domain (#67)
    * Add new job subscriber domain model to separate client domain from subscriber domain
    
    * Revert jobs namespace back to fleet_cmdr to maintain backwards compatibility with release client binaries
    
    * Add subscriber ID inside JobExecutionSubscriberUpdate to support demuxing inside doc-server
    
    * Add JobCancel message and allow JobRequestNext to include ignored execution IDs so that jobs can be parallelized
    
    * Add should_cancel bool to the JobExecution message in case a device misses the JobCancel notification

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -53,8 +53,8 @@ pub mod config {
     tonic::include_proto!("config");
 }
 
-pub mod jobs {
+pub mod fleet_cmdr {
     pub mod v1 {
-        tonic::include_proto!("jobs.v1");
+        tonic::include_proto!("fleet_cmdr.v1");
     }
 }
