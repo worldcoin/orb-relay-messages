d0c339983a9550f59096ff02bc433b334c4876d4
Melanie Molina <54809973+melmols@users.noreply.github.com>
2026-09-15 21:11:35 +0200

    ORBA-666: Remove redundant iPCP ciphertext hash field

--- rust/tests/ipcp_pairing_messages.rs
+++ rust/tests/ipcp_pairing_messages.rs
@@ -28,7 +28,6 @@ fn ipcp_pairing_fields_round_trip() {
     };
     let app_announcement = AnnounceAppId {
         encrypted_ipcp: vec![13, 14, 15],
-        encrypted_ipcp_hash: vec![16, 17, 18],
         orb_nonce: vec![4, 5, 6],
         integrity_token: "integrity-token".into(),
         integrity_signature: vec![19, 20, 21],
@@ -48,4 +47,39 @@ fn ipcp_pairing_fields_round_trip() {
 #[test]
 fn pairing_request_rejects_truncated_request_nonce() {
     assert!(PairingRequest::decode([0x22, 0x80].as_slice()).is_err());
+}
+
+#[test]
+fn app_announcement_ignores_removed_hash_field() {
+    let expected = AnnounceAppId {
+        encrypted_ipcp: vec![13, 14, 15],
+        orb_nonce: vec![4, 5, 6],
+        integrity_token: "integrity-token".into(),
+        integrity_signature: vec![19, 20, 21],
+        integrity_timestamp: 1_725_302_400,
+        ..Default::default()
+    };
+    let mut legacy_bytes = expected.encode_to_vec();
+    legacy_bytes.extend_from_slice(&[0x4a, 0x03, 16, 17, 18]);
+
+    let decoded = AnnounceAppId::decode(legacy_bytes.as_slice()).unwrap();
+    assert_eq!(decoded, expected);
+    assert_eq!(decoded.encode_to_vec(), expected.encode_to_vec());
+}
+
+#[test]
+fn app_announcement_preserves_remaining_field_numbers() {
+    let announcement = AnnounceAppId {
+        encrypted_ipcp: vec![1],
+        orb_nonce: vec![2],
+        integrity_token: "jwt".into(),
+        integrity_signature: vec![3],
+        integrity_timestamp: 1,
+        ..Default::default()
+    };
+
+    assert_eq!(
+        announcement.encode_to_vec(),
+        [0x42, 1, 1, 0x52, 1, 2, 0x5a, 3, b'j', b'w', b't', 0x62, 1, 3, 0x68, 1]
+    );
 }
